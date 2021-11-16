e124811515ce12b916b520e92f08bfac99d20726
Sergio <sdiaz@us.es>
2021-11-16 19:00:03 +0100

    New and modified keys, template now allows directories
    - Added SESSION, LOC_ISO_DATE keys and renamed DATEOBS to
    LOC_ISO_DATETIME
    - filename_template allows path separators (to specify subdirectories)

--- batch_fits_rename.py
+++ batch_fits_rename.py
@@ -5,7 +5,7 @@ import argparse
 import sys
 import os
 import re
-from datetime import datetime, timezone
+from datetime import datetime, timezone, time, timedelta
 from astropy.io import fits
 from xisf import XISF
 from glob import iglob 
@@ -13,8 +13,16 @@ from glob import iglob
 DESC="""
 Batch rename FITS files in the specified path, using a template based on custom text and FITS keyword values.
 
-Example: 
-  batch_fits_rename.py e:\Astrofoto\_Flats\**\*.fits '{FRAME}_{FILTER}_{DATEOBS}_{SEQ}{EXT}' -r
+Examples: 
+- Basic usage:
+  ```
+  batch_fits_rename.py E:\Astro\Flats\**\*.fits '{FRAME}_{FILTER}_{DATEOBS}_{SEQ}{EXT}' -r`
+  ```
+
+- Renaming and organizing in subdirectories in one go:
+  ```
+  batch_fits_rename.py Light\**\*.fits "SESSION_{SESSION}\LionNeb_Light_{FILTER}_600secs_{LOC_ISO_DATETIME}_{SEQ}{EXT}" -r 
+  ```
 """
 
 # Debug
@@ -29,24 +37,27 @@ parser.add_argument("path_to_fits_or_xisf_files",
                     help="Path to the FITS or XISF files, using wildcards. With -r, you may also specify '**' " +
                     "to expand for any number of directory levels.")
 parser.add_argument("filename_template", 
-                    help="String that specifies the target filenames, combining fixed text and FITS keywords, " + 
-                    "between curly braces. Also available as keywords are " + 
-                    "DATEOBS (local datetime), SEQ (sequence number, if the original filename had it) " + 
-                    "and EXT (original file extension, including the '.')")
+                    help="String that specifies the target filenames, combining fixed text and " + 
+                    "FITS keywords between curly braces. Also available as keywords are " + 
+                    "LOC_ISO_DATETIME (local datetime), LOC_ISO_DATE (local date), " +
+                    "SESSION (date 'rounded' to the nearest midnight), " + 
+                    "SEQ (sequence number, if the original filename had it) " + 
+                    "and EXT (original file extension, including the '.')." +
+                    "This template could include pathname separators: in this case, the directories " + 
+                    "will be automatically created. See the examples.")
 parser.add_argument("-d", "--dry-run", action='store_true',
                     help="Dry run")
 parser.add_argument("-r", "--recursive", action='store_true',
                     help="Recurse directories")
 args = parser.parse_args()
 
-
-def get_fits_keyword_values(header, keys):
-    return [str(header[k]) if k in hdr else 'MISSING' for k in key]
-
-
 # FITS path
 files_path = args.path_to_fits_or_xisf_files
 recursive = args.recursive
+
+
+def get_fits_keyword_values(header, keys):
+    return [str(header[k]) if k in hdr else 'MISSING' for k in key]
 
 
 def get_file_keywords(fname):
@@ -91,11 +102,18 @@ def augment_keywords(hdr, fname):
     except:
         hdr['SEQ'] = "NULLSEQ"
 
-    # Add DATEOBS as local time
-    hdr['DATEOBS'] = datetime.fromisoformat(hdr['DATE-OBS']).replace(tzinfo=timezone.utc).astimezone(tz=None).isoformat()[:19]
+    # Add local time
+    dateobs_dt = datetime.fromisoformat(hdr['DATE-OBS']).replace(tzinfo=timezone.utc).astimezone(tz=None)
+    dateobs_iso = dateobs_dt.isoformat()
+    hdr['LOC_ISO_DATETIME'] = dateobs_iso[:19]
+    hdr['LOC_ISO_DATE'] = dateobs_iso[:10]
 
     # Trim DATE-OBS (UTC) to second accuracy
     hdr['DATE-OBS'] = hdr['DATE-OBS'][:19]
+
+    # Derive SESSION identifier from DATEOBS, "rounding" to the nearest midnight
+    session_date = dateobs_dt if dateobs_dt.time() < time(12, 00) else dateobs_dt + timedelta(days=1)
+    hdr['SESSION'] =  session_date.strftime("%Y%m%d")
 
     # If FILTER is missing (KStars/Ekos bug), use the current directory
     if 'FILTER' not in hdr:
@@ -105,7 +123,7 @@ def augment_keywords(hdr, fname):
 
 def to_safe_fname(s):
     s = s.replace(':', '-')
-    return "".join( c for c in s if (c.isalnum() or c in "._- "))
+    return "".join( c for c in s if (c.isalnum() or c in "._- \\"))
 
 
 def to_filename(tpl, hdr):
@@ -114,8 +132,9 @@ def to_filename(tpl, hdr):
     
 keys = get_keywords_in_tpl(args.filename_template)
 prev_dir = ''
-fname_gen = ( fname for fname in iglob(files_path, recursive=recursive) if os.path.isfile(fname) )
-for fname in fname_gen:
+# We snapshot the fnames here to avoid recursion (renaming the renamed files!)
+fname_list = [fname for fname in iglob(files_path, recursive=recursive) if os.path.isfile(fname)]
+for fname in fname_list:
     hdr = get_file_keywords(fname)
     augment_keywords(hdr, fname)
 
@@ -133,6 +152,7 @@ for fname in fname_gen:
     new_fname = os.path.join(cur_dir, new_fname)
 
     if not args.dry_run:
+        os.makedirs(os.path.dirname(new_fname), exist_ok=True)
         os.rename(fname, new_fname)
         print("done.")
     else:
